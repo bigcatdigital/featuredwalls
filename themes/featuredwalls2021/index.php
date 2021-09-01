dd220ba8a4d7f6c428d63e9abf9d92b76545597e
bigcatdigital <mmason2@hotmail.com>
2021-09-01 23:17:31 +0100

    Resin bound pavements

--- themes/featuredwalls2021/index.php
+++ themes/featuredwalls2021/index.php
@@ -6,7 +6,7 @@
 	<title>Featured Walls and Ceilings | Homepage</title>
 	<meta name="description" content="Featured Walls and Ceilings: creating beautiful spaces with custom interior and exterior design specialists. Serving north-west of Ireland, Northern Ireland and all of the island of Ireland.">
 	<link href="https://fonts.googleapis.com/css?family=Alegreya|Alegreya+Sans|Merriweather|Merriweather+Sans|Nunito|Nunito+Sans|Quattrocento|Quattrocento+Sans|Roboto|Roboto+Mono|Roboto+Slab&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="https://use.typekit.net/lwq7zun.css">
+
 	<!-- Google fonts Oswald & Lato -->
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -15,7 +15,6 @@
 	<?php 
     echo '<link rel="stylesheet" href="' . './assets/css/style.css?t=s' . microtime() . '" />';
   ?>
-	
 </head>  
 	<body>
 		<div class="bc-body-wrap">
@@ -24,7 +23,7 @@
 			</nav>
 			<header class="bc-site-header bc-is-hero-overlay bc-container">
 				<a title="Site hompepage" class="bc-site-header__home-link" href="/"> 
-					<img src="./assets/media/caps-minimal-lc-final.svg" alt="CAPS Ltd logo - presenting Featured Walls and Ceilings" />
+					<img src="./assets/media/caps-minimal-lc-final.png" alt="CAPS Ltd logo - presenting Featured Walls and Ceilings" /> 
 				</a>
 				<a href="#main-site-content" class="bc-site-header__menu-skip bc-is-hidden" title="Skip site menu, to main site content"></a>
 				<a href="javascript:void(0)" aria-hidden="true" class="bc-site-header__menu-link" title="Site Menu">
@@ -43,7 +42,7 @@
 				</ul>	
 			</header><!-- // ..be-hero__header -->
 			<!-- Hero -->
-			<section id="main-site-content" class="bc-hero has-lines" aria-label="Welcome to Featured Walls and Ceilings">
+			<section id="main-site-content" class="bc-hero--full-screen  has-lines" aria-label="Welcome to Featured Walls and Ceilings">
 				<picture class="bc-hero__media">
 					<img src="assets/media/featured-walls-and-ceilings-wood-finish.jpg" alt="A featured wall installation in a beautifully designed interior" />
 				</picture>
@@ -110,7 +109,7 @@
 			</section>
 			<!-- // Flickity hero slider -->
 			<!-- Sensory rooms -->
-			<section id="body-content" class="bc-container " aria-label="Find out about our stylish featured walls and Ceilings">
+			<section id="sensory-rooms" class="bc-container " aria-label="Find out about our stylish featured walls and Ceilings">
 				<div class="bc-text-component bc-content-block bc-column bc-featured-intro"> 
 					<h1 class="bc-content-block__heading"><span class="bc-featured-intro__heading">Customized sensory rooms</span></h1> 
 					<div class="bc-featured-intro__body">
@@ -118,96 +117,115 @@
 						<p>We design and install our unique <i>star ceilings</i> and <i>LED ceiling lights</i> to help our customers do just that to great effect. </p>
 					</div>
 				</div><!-- .bc-featured-intro -->
-				<div class="bc-grid bc-2-col-slider"> 
-					<div class="bc-2-col-slider__intro"> 
-						<h2>Star ceilings</h2>
-						<p>Add colour, life and stimulate imagination with a custom Star Ceiling.</p> 
-					</div>
-					<!-- slider left -->
-					<div class="bc-flickity">
+				<div class="bc-grid--x2 bc-2-col-sliders bc-media-component">
+					<!-- slider-left -->
+					<div class="bc-2-col-slider"> 
+						<!-- slider left -->
+						<div class="bc-flickity">
+							<span class="frame frame--top"></span> 
+							<span class="frame frame--bottom"></span>
+							<article class="bc-flickity__slide is-object-contain" aria-label="Hero slide #1">	
+								<picture class="">
+									<img src="assets/media/star-ceiling-lights-2.jpg" alt="A star ceiling lighting design to create a relaxing experience in your room" />
+								</picture><!-- // .bc-hero__media -->
+							</article><!-- // .bc-flickity__slide -->
+							<article class="bc-flickity__slide is-object-contain" aria-label="Hero slide #3">
+								<picture class="">
+									<img src="assets/media/star-ceiling-lights-3.jpg" alt="A star ceiling lighting design to create a relaxing experience in your room" />
+								</picture><!-- // .bc-hero__media --> 
+							</article><!-- // .bc-flickity__slide -->
+							<article class="bc-flickity__slide is-object-contain" aria-label="Hero slide #1">
+								<picture class=""> 
+									<img src="assets/media/star-ceiling-lights-1.jpg" alt="A star ceiling lighting design to create a relaxing experience in your room" />
+								</picture><!-- // .bc-hero__media -->
+							</article><!-- // .bc-flickity__slide -->
+						</div><!-- //.bc-flickity -->
+						<div class="bc-2-col-slider__intro"> 
+							<h2>Star ceilings</h2>
+							<p>Add colour, life and stimulate imagination with a custom Star Ceiling.</p> 
+						</div><!-- // .bc-2-col-slider__intro -->
+					</div><!-- // slider left -->
+					<!-- slider right -->
+					<div class="bc-2-col-slider">
+						<div class="bc-flickity">
+							<span class="frame frame--top"></span>
+							<span class="frame frame--bottom"></span>
+							<article class="bc-flickity__slide is-object-contain" aria-label="Hero slide #1">
+								<picture class=""> 
+									<img src="assets/media/ceiling-led-lights-1.jpg" alt="A ceiling LED lighting installation to add atmosphere to a room" />
+								</picture><!-- // .bc-hero__media -->
 
-						<span class="frame frame--top"></span> 
-						<span class="frame frame--bottom"></span>
+							</article><!-- // .bc-flickity__slide -->
+							<article class="bc-flickity__slide is-object-contain" aria-label="Hero slide #1">
+								<picture class="">
+									<img src="assets/media/ceiling-led-lights-2.jpg" alt="A custom featured wall with inset fireplace and lighting" />
+								</picture><!-- // .bc-hero__media -->
 
-						<article class="bc-flickity__slide is-object-contain" aria-label="Hero slide #1">	
-							<picture class="">
-								<img src="assets/media/star-ceiling-lights-2.jpg" alt="A star ceiling lighting design to create a relaxing experience in your room" />
-							</picture><!-- // .bc-hero__media -->
-						</article><!-- // .bc-flickity__slide -->
-						<article class="bc-flickity__slide is-object-contain" aria-label="Hero slide #3">
-							<picture class="">
-								<img src="assets/media/star-ceiling-lights-3.jpg" alt="A star ceiling lighting design to create a relaxing experience in your room" />
-							</picture><!-- // .bc-hero__media --> 
-						</article><!-- // .bc-flickity__slide -->
-						<article class="bc-flickity__slide is-object-contain" aria-label="Hero slide #1">
-							<picture class=""> 
-								<img src="assets/media/star-ceiling-lights-1.jpg" alt="A star ceiling lighting design to create a relaxing experience in your room" />
-							</picture><!-- // .bc-hero__media -->
-						</article><!-- // .bc-flickity__slide -->
-					</div><!-- //.bc-flickity -->
-				<!-- // slider left -->
-				</div>
-				<div class="bc-grid bc-2-col-slider">
-				<!-- slider right -->
-					<div class="bc-2-col-slider__intro">
-						<h2>LED ceiling lights</h2>
-						<p>Bring calm and atomosphere to any space with our uniquely designed LED ceiling lights units.</p> 
+							</article><!-- // .bc-flickity__slide -->
+							<article class="bc-flickity__slide is-object-contain" aria-label="Hero slide #3">
+								<picture class="">
+
+									<img src="assets/media/ceiling-led-lights-2.jpg" alt="A ceiling LED lighting installation to add atmosphere to a room" />
+								</picture><!-- // .bc-hero__media --> 
+
+							</article><!-- // .bc-flickity__slide -->
+							<article class="bc-flickity__slide is-object-contain" aria-label="Hero slide #3">
+								<picture class="">
+									<img src="assets/media/ceiling-led-lights-3.jpg" alt="A ceiling LED lighting installation to add atmosphere to a room" />
+								</picture><!-- // .bc-hero__media -->
+							</article><!-- // .bc-flickity__slide -->
+							<article class="bc-flickity__slide is-object-contain" aria-label="Hero slide #3">
+								<picture class="">
+									<img src="assets/media/ceiling-led-lights-4.jpg" alt="A ceiling LED lighting installation to add atmosphere to a room" />
+								</picture><!-- // .bc-hero__media -->
+							</article><!-- // .bc-flickity__slide -->
+							<article class="bc-flickity__slide is-object-contain" aria-label="Hero slide #3">
+								<picture class="">
+									<img src="assets/media/ceiling-led-lights-5.jpg" alt="A ceiling LED lighting installation to add atmosphere to a room" />
+								</picture><!-- // .bc-hero__media -->
+							</article><!-- // .bc-flickity__slide -->
+							<article class="bc-flickity__slide is-object-contain" aria-label="Hero slide #3">
+								<picture class="">
+									<img src="assets/media/ceiling-led-lights-6.jpg" alt="A ceiling LED lighting installation to add atmosphere to a room" />
+								</picture><!-- // .bc-hero__media -->
+							</article><!-- // .bc-flickity__slide -->
+						</div><!-- //.bc-flickity -->
+						<div class="bc-2-col-slider__intro">
+							<h2>LED ceiling lights</h2>
+							<p>Bring calm and atomosphere to any space with our uniquely designed LED ceiling lights units.</p> 
+						</div>
 					</div>
-					<div class="bc-flickity">
-						<span class="frame frame--top"></span>
-						<span class="frame frame--bottom"></span>
-						<article class="bc-flickity__slide is-object-contain" aria-label="Hero slide #1">
-							<picture class=""> 
-								<img src="assets/media/ceiling-led-lights-1.jpg" alt="A ceiling LED lighting installation to add atmosphere to a room" />
-							</picture><!-- // .bc-hero__media -->
-							
-						</article><!-- // .bc-flickity__slide -->
-						<article class="bc-flickity__slide is-object-contain" aria-label="Hero slide #1">
-							<picture class="">
-								<img src="assets/media/ceiling-led-lights-2.jpg" alt="A custom featured wall with inset fireplace and lighting" />
-							</picture><!-- // .bc-hero__media -->
-							
-						</article><!-- // .bc-flickity__slide -->
-						<article class="bc-flickity__slide is-object-contain" aria-label="Hero slide #3">
-							<picture class="">
-								
-								<img src="assets/media/ceiling-led-lights-2.jpg" alt="A ceiling LED lighting installation to add atmosphere to a room" />
-							</picture><!-- // .bc-hero__media --> 
-							
-						</article><!-- // .bc-flickity__slide -->
-						<article class="bc-flickity__slide is-object-contain" aria-label="Hero slide #3">
-							<picture class="">
-								<img src="assets/media/ceiling-led-lights-3.jpg" alt="A ceiling LED lighting installation to add atmosphere to a room" />
-							</picture><!-- // .bc-hero__media -->
-						</article><!-- // .bc-flickity__slide -->
-						<article class="bc-flickity__slide is-object-contain" aria-label="Hero slide #3">
-							<picture class="">
-								<img src="assets/media/ceiling-led-lights-4.jpg" alt="A ceiling LED lighting installation to add atmosphere to a room" />
-							</picture><!-- // .bc-hero__media -->
-						</article><!-- // .bc-flickity__slide -->
-						<article class="bc-flickity__slide is-object-contain" aria-label="Hero slide #3">
-							<picture class="">
-								<img src="assets/media/ceiling-led-lights-5.jpg" alt="A ceiling LED lighting installation to add atmosphere to a room" />
-							</picture><!-- // .bc-hero__media -->
-						</article><!-- // .bc-flickity__slide -->
-						<article class="bc-flickity__slide is-object-contain" aria-label="Hero slide #3">
-							<picture class="">
-								<img src="assets/media/ceiling-led-lights-6.jpg" alt="A ceiling LED lighting installation to add atmosphere to a room" />
-							</picture><!-- // .bc-hero__media -->
-						</article><!-- // .bc-flickity__slide -->
-					</div><!-- //.bc-flickity -->
-				</div>
-				<!-- // slider right -->
-			</div><!-- // .bc-grid--x2 -->
+					<!-- // slider right -->
+				</div><!-- // .bc-2-col-sliders -->
 			</section>
-			<!-- // Flickity hero slider -->
-			<section class="bc-container bc-get-started">
+			<!-- Resin bound driveways -->
+			<section id="resin-bound" class="bc-container bc-hero bc-hero--in-page">
+				<picture class="bc-hero__media">
+					<img src="assets/media/resin-bound-compass-2.jpg" alt="A resin bound driveway with embedded compass design" />
+				</picture>
+				
+				<article class="bc-hero__body" aria-label="Main page hero body">
+					<div class="bc-hero__body__content bc-text-component">
+						<h1 class="bc-hero__heading">Resin bound driveways</h1>
+						<p class="bc-hero__tagline">Elegant, long lasting, durable, beautiful custom designed resin bound driveways</p>
+						<p><i>What if you could have a custom designed durable, safe walkway, driveway or patio added to your home or work premises?</i></p>
+						<p class="bc-arrow-link">
+						<a href="./resin-bound-driveways.php">Find out more here</a>				
+							<svg class="svg-icon">
+								<use xlink:href="assets/media/svg/icons/bc-svgs.svg#arrow"></use>
+							</svg>	
+						</p>
+					</div>
+				</article>
+			</section>
+			<!-- // Resin bound driveways -->
+			<section id="get-started" class="bc-container bc-get-started">
 				<article class="bc-grid--x2">
 					<div class="bc-get-started__column">
 						<h1>Get started</h1>
 						<p>Contact us now by email, phone or on social media to get started.</p>
 					</div><!-- // bc-grid column-->
-					<div class="bc-get-started__column">
+					<div class="bc-get-started__column"> 
 						<p class="bc-icon-link">
 							<svg class="svg-icon">
 								<use xlink:href="assets/media/svg/icons/bc-svgs.svg#phone-outline-2"></use>
@@ -227,9 +245,8 @@
 							<a href="mailto:capsltd@hotmail.com">capsltd@hotmail.com</a>
 						</p>
 					</div><!-- // bc-grid column-->
-					
 				</article><!-- .bc-grid--x2 -->
-			</section>
+			</section><!-- // Get started -->
 			
 			<footer class="bc-footer bc-container"> 
 				<div class="bc-grid">
@@ -241,16 +258,10 @@
 							<a href="tel:01234567">01234567</a><br />
 						</p>
 					</div><!-- // grid column -->
-					<div>
-
-					</div><!-- // grid column -->
-					
 				</div><!-- // .bc-grid -->
 			</footer>
-			<script src="assets/scripts/app-concat.js"></script>  
-      <?php 
-        echo 'script src="assets/scripts/app-concat.js' . microtime() . '"></script>';
-      ?>
 		</div><!-- //. bc-body-wrap --> 
+		<script src="assets/scripts/app-concat.js"></script>  
+    <?php echo 'script src="assets/scripts/app-concat.js' . microtime() . '"></script>'; ?>
 	</body>
 </html> 
---
+++ themes/featuredwalls2021/resin-bound-driveways.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Featured Walls and Ceilings | Homepage</title>
+	<meta name="description" content="Featured Walls and Ceilings: we design and lay custom, unique, beautiful resin bound driveways for our clients in the northwest, Northern Ireland and all of the island of Ireland.">
+	<link href="https://fonts.googleapis.com/css?family=Alegreya|Alegreya+Sans|Merriweather|Merriweather+Sans|Nunito|Nunito+Sans|Quattrocento|Quattrocento+Sans|Roboto|Roboto+Mono|Roboto+Slab&display=swap" rel="stylesheet">
+	<!-- Google fonts Oswald & Lato -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Oswald:wght@200;400;500;600&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="./assets/css/vendor/flickity.min.css" />
+	<?php 
+    echo '<link rel="stylesheet" href="' . './assets/css/style.css?t=s' . microtime() . '" />';
+  ?>
+</head>  
+<body>
+<div class="bc-body-wrap">
+	<nav class="bc-is-hidden" aria-label="Skip to main content link" >
+		<a href="#main-site-content" title="Skip site menu, go to to main site content"></a>	
+	</nav>
+	<header class="bc-site-header bc-container">
+    <a title="Site hompepage" class="bc-site-header__home-link" href="/"> 
+		  <img src="./assets/media/caps-minimal-lc-final.png" alt="CAPS Ltd logo - presenting Featured Walls and Ceilings" /> 
+		</a>
+		<a href="#main-site-content" class="bc-site-header__menu-skip bc-is-hidden" title="Skip site menu, to main site content"></a>
+		<a href="javascript:void(0)" aria-hidden="true" class="bc-site-header__menu-link" title="Site Menu">
+			<svg class="svg-icon bc-site-header__menu-link__menu ">
+				<use xlink:href="assets/media/svg/icons/bc-svgs.svg#menu-thin"></use>
+			</svg>
+			<svg class="svg-icon bc-site-header__menu-link__close "> 
+				<use xlink:href="assets/media/svg/icons/bc-svgs.svg#close-x"></use>
+			</svg>
+		</a>
+		<ul id="site-main-navigation" role="navigation" aria-label="Main site navigation" class="bc-site-header__main-navigation">
+			<li class="bc-site-header__main-navigation__item"><a href="single-page.html">Single Page</a></li>
+			<li class="bc-site-header__main-navigation__item"><a href="javascript:void(0)">Page #2</a></li>
+			<li class="bc-site-header__main-navigation__item"><a href="javascript:void(0)">Page #3</a></li>
+			<li class="bc-site-header__main-navigation__item"><a href="javascript:void(0)">Page #4</a></li>
+		</ul>	
+	</header><!-- // ..be-hero__header -->
+	<!-- Hero -->
+	
+	<!-- About resin bound driveways-->
+  <section id="body-content" class="bc-container " aria-label="Find out about our stylish featured walls and Ceilings">
+		<div class="bc-text-component bc-content-block bc-column">
+			<h1>Resin bound driveways</h1>
+			<p class=""><strong>Elegant, long lasting, durable, beautiful.</strong></p>	
+			<p>We we create custom designed resin bound surfaces for driveways, pathways, patios, walkways...anywhere a stone or tarmac surface can be used.</p>
+      <p><a class="bc-scroll-link" href="#resin-body-content"><i>Find out why and how below here.</i></a></p>
+		</div>
+		<div class="bc-media-component bc-column">
+      <picture class="">
+        <img src="assets/media/resin-bound-compass-2.jpg" alt="A resin bound driveway with embedded compass design" />
+      </picture>
+    </div>
+    <div class="bc-text-component" id="resin-body-content">
+      <h2>What is is?</h2>
+      <p>It is a surfacing material that is created by mixing <strong>resin</strong> and <strong>aggregate stone</strong>. The mixture is then laid onto the surface to create a strong, clear and permeable finish. It is a popular option for footpaths, patios, walkways and driveways.</p>
+      <p>Some people call it “<i>stone carpet</i>”, it provides a seamless finish which is durable and has no loose stones or weeds to remove. </p> 
+      <p>It&apos;s <strong>permeable</strong> too, as the mixing process creates small gaps between the composite aggregates, this supports drainage requirements. That&apos; good news in itself but it also avoids any need for planning permission that may be required for other surfaces.</p>
+      <p>Resin bound surfaces can even be laid over existing driveways, car parks and other existing stone pavements. </p>
+      <h2>Why use it?</h2>
+      <p>It is durable and permeable and can be used where a stone finish is needed. It has improved slip resistance. It is also slip resistant, weed and frost resistant and colour-stable.</p>
+      <h2>The design possibilities</h2>
+      <p>They’re almost limitless, any shape can be created. There are standard colour combinations and we can also work to make an almost limitless range of new colours to suit your needs.</p>
+      <p>This wide range of colours and blends make for unique design options and it makes for a beautiful alternative to traditional outdoor surfaces. </p>
+      <p><strong>Sound like your thing?</strong></p>
+      
+      <p><a class="bc-scroll-link" href="#get-started">Get in touch with us here and tell us what you&apos;re thinking</a>.</p>
+      <p>We will find a design solution that works for you.</p>
+      <p class="bc-icon-link">
+        <svg class="svg-icon">
+					<use xlink:href="assets/media/svg/icons/bc-svgs.svg#arrow--left"></use>
+				</svg>
+        <a href="./">Back to our homepage</a>	
+      </p>
+    </div>   
+	</section>
+	<!-- // About resin bound driveways -->
+  <section id="get-started" class="bc-container bc-get-started">
+		<article class="bc-grid--x2">
+			<div class="bc-get-started__column">
+				<h1>Get started</h1>
+				<p>Contact us now by email, phone or on social media to get started.</p>
+			</div><!-- // bc-grid column-->
+			<div class="bc-get-started__column">
+				<p class="bc-icon-link">
+					<svg class="svg-icon">
+						<use xlink:href="assets/media/svg/icons/bc-svgs.svg#phone-outline-2"></use>
+					</svg>	
+					<a href="tel:0863627302">086 362 7302</a>
+				</p>
+				<p class="bc-icon-link">
+					<svg class="svg-icon">
+						<use xlink:href="assets/media/svg/icons/bc-svgs.svg#facebook"></use>
+					</svg>	
+					<a href="https://www.facebook.com/featuredbespokewallsandceilings">Featured Walls and Ceilings</a>
+				</p>
+				<p class="bc-icon-link">
+					<svg class="svg-icon">
+						<use xlink:href="assets/media/svg/icons/bc-svgs.svg#email-at"></use>
+					</svg>	
+					<a href="mailto:capsltd@hotmail.com">capsltd@hotmail.com</a>
+				</p>
+			</div><!-- // bc-grid column-->
+		</article><!-- .bc-grid--x2 -->
+	</section><!-- // Get started -->
+	<footer class="bc-footer bc-container"> 
+		<div class="bc-grid">
+			<div>
+				<p>
+					<strong>CAPSLtd</strong>,<br />
+					Greencastle<br />
+					Co. Donegal<br />
+					<a href="tel:01234567">01234567</a><br />
+				</p>
+			</div><!-- // grid column -->
+		</div><!-- // .bc-grid -->
+	</footer>
+  </div><!-- //. bc-body-wrap --> 
+  <script src="assets/scripts/app-concat.js"></script>  
+  <?php echo 'script src="assets/scripts/app-concat.js' . microtime() . '"></script>'; ?>
+</body>
+</html> 
