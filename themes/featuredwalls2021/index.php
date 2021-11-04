9f34058afa4b8bb31eece784859e2c5b981290a4
bigcatdigital <mmason2@hotmail.com>
2021-11-04 16:02:50 +0000

    Edits to major site CTA links

--- themes/featuredwalls2021/index.php
+++ themes/featuredwalls2021/index.php
@@ -25,7 +25,7 @@
 		window.dataLayer = window.dataLayer || [];
 		function gtag(){dataLayer.push(arguments);}
 		gtag('js', new Date());
-		gtag('config', 'G-EB511QMQDJ');
+		gtag('config', 'G-EB511QMQDJ'); 
 	</script>
 </head>  
 	<body>
@@ -139,8 +139,8 @@
 							<h2>Custom sensory room design</h2>
 							<p>Add colour, life and a calming experience with a customised sensory room.</p> 
 							<p class="bc-arrow-link">
-								<a href="resin-driveways/">Find out more here</a>				
-								<svg class="svg-icon">
+								<a href="sensory-rooms/">Find out more here</a>				
+								<svg class="svg-icon"> 
 									<use xlink:href="assets/media/svg/icons/bc-svgs.svg#arrow"></use>
 								</svg>	
 							</p><!-- // .bc-arrow-link -->
@@ -173,31 +173,54 @@
 						<p>Contact us now by email, phone or on social media to get started.</p>
 					</div><!-- // bc-grid column-->
 					<div class="bc-column bc-text-component bc-get-started__links">
-						<p class="bc-icon-link">
-							<svg class="svg-icon">
-								<use xlink:href="assets/media/svg/icons/bc-svgs.svg#phone-outline-2"></use>
-							</svg>	
-							<a href="tel:0863627302">086 362 7302</a>
-						</p>
-						<p class="bc-icon-link">
-							<svg class="svg-icon">
-								<use xlink:href="assets/media/svg/icons/bc-svgs.svg#facebook"></use>
-							</svg>	
-							<a href="https://www.facebook.com/resinbounddesigns">Resin Bound Designs Ireland</a>
-						</p>
-						<p class="bc-icon-link">
-							<svg class="svg-icon">
-								<use xlink:href="assets/media/svg/icons/bc-svgs.svg#facebook"></use>
-							</svg>	
-							<a href="https://www.facebook.com/featuredbespokewallsandceilings">Featured Walls and Ceilings</a>
-						</p>
-						
-						<p class="bc-icon-link">
-							<svg class="svg-icon">
-								<use xlink:href="assets/media/svg/icons/bc-svgs.svg#email-at"></use>
-							</svg>	
-							<a href="mailto:capsltd@hotmail.com">capsltd@hotmail.com</a>
-						</p>
+						<div class="bc-get-started__column">
+							<div class="bc-get-started__link">
+								<span class="bc-get-started__button-wrap">
+									<a href="tel:0863627302"class="bc-get-started__button bc-call-to-action-button">
+										<svg class="svg-icon">
+											<use xlink:href="assets/media/svg/icons/bc-svgs.svg#phone-outline-2"></use>
+										</svg>	
+										<span class="bc-get-started__button__text">086 362 7302</span>	 	
+									</a><!-- // .bc-get-started__button -->	
+								</span><!-- // .bc-get-started__button-wrap -->	
+							</div><!-- // .bc-get-started__link -->
+						</div><!-- // .bc-get-started__column -->
+						<div class="bc-get-started__column">
+							<div class="bc-get-started__link">
+								<span class="bc-get-started__button-wrap">
+									<a href="https://www.facebook.com/resinbounddesigns"class="bc-get-started__button bc-call-to-action-button">
+										<svg class="svg-icon">
+											<use xlink:href="assets/media/svg/icons/bc-svgs.svg#facebook"></use>
+										</svg>		
+									<span class="bc-get-started__button__text">Resin Bound Designs Ireland</span>	
+									</a><!-- // .bc-get-started__button -->	
+								</span><!-- // .bc-get-started__button-wrap -->	
+							</div><!-- // .bc-get-started__link -->
+						</div><!-- // .bc-get-started__column -->
+						<div class="bc-get-started__column">
+							<div class="bc-get-started__link">
+								<span class="bc-get-started__button-wrap">
+									<a href="https://www.facebook.com/featuredbespokewallsandceilings"class="bc-get-started__button bc-call-to-action-button">
+										<svg class="svg-icon">
+											<use xlink:href="assets/media/svg/icons/bc-svgs.svg#facebook"></use>
+										</svg>		
+										<span class="bc-get-started__button__text">Featured Walls and Ceilings</span>
+									</a><!-- // .bc-get-started__button -->	
+								</span><!-- // .bc-get-started__button-wrap -->	
+							</div><!-- // .bc-get-started__link -->
+						</div><!-- // .bc-get-started__column -->
+						<div class="bc-get-started__column">
+							<div class="bc-get-started__link">
+								<span class="bc-get-started__button-wrap">
+									<a href="mailto:capsltd@hotmail.com" class="bc-get-started__button bc-call-to-action-button">
+										<svg class="svg-icon">
+											<use xlink:href="assets/media/svg/icons/bc-svgs.svg#email-at"></use>
+										</svg>		
+										<span class="bc-get-started__button__text">capsltd@hotmail.com</span> 
+									</a><!-- // .bc-get-started__button -->	
+								</span><!-- // .bc-get-started__button-wrap -->	
+							</div><!-- // .bc-get-started__link -->
+						</div><!-- // .bc-get-started__column -->
 					</div><!-- // bc-grid column-->
 				</article><!-- .bc-grid--x2 -->
 			</section><!-- // Get started -->
--- themes/featuredwalls2021/resin-driveways/index.php
+++ themes/featuredwalls2021/resin-driveways/index.php
@@ -105,43 +105,63 @@
 	</section>
 	<!-- // About resin bound driveways -->
   <section id="get-started" class="bc-container bc-get-started">
-		<article class="">
-			<div class="bc-content-component--text bc-content-block bc-column">
-				<div class="bc-text-block">
-					<h1>Get started</h1>
-					<p>Contact us now by email, phone or on social media to get started.</p>
-				</div>
-				
-			</div><!-- // .bc-content-component-->
-			<div class="bc-column bc-content-component bc-get-started__links">
-				<p class="bc-icon-link">
-					<svg class="svg-icon">
-						<use xlink:href="../assets/media/svg/icons/bc-svgs.svg#phone-outline-2"></use>
-					</svg>	
-					<a href="tel:0863627302">086 362 7302</a>
-				</p>
-				<p class="bc-icon-link">
-					<svg class="svg-icon">
-						<use xlink:href="../assets/media/svg/icons/bc-svgs.svg#facebook"></use>
-					</svg>	
-					<a href="https://www.facebook.com/resinbounddesigns">Resin Bound Designs Ireland</a>
-				</p>
-				<p class="bc-icon-link">
-					<svg class="svg-icon">
-						<use xlink:href="../assets/media/svg/icons/bc-svgs.svg#facebook"></use>
-					</svg>	
-					<a href="https://www.facebook.com/featuredbespokewallsandceilings">Featured Walls and Ceilings</a>
-				</p>
-				
-				<p class="bc-icon-link">
-					<svg class="svg-icon">
-						<use xlink:href="../assets/media/svg/icons/bc-svgs.svg#email-at"></use>
-					</svg>	
-					<a href="mailto:capsltd@hotmail.com">capsltd@hotmail.com</a>
-				</p>
-			</div><!-- // bc-grid column-->
-		</article><!-- .bc-grid--x2 -->
-	</section><!-- // Get started -->
+				<article class="">
+					<div class="bc-text-component bc-content-block bc-column">
+						<h1>Get started</h1>
+						<p>Contact us now by email, phone or on social media to get started.</p>
+					</div><!-- // bc-grid column-->
+					<div class="bc-column bc-text-component bc-get-started__links">
+						<div class="bc-get-started__column">
+							<div class="bc-get-started__link">
+								<span class="bc-get-started__button-wrap">
+									<a href="tel:0863627302"class="bc-get-started__button bc-call-to-action-button">
+										<svg class="svg-icon">
+											<use xlink:href="../assets/media/svg/icons/bc-svgs.svg#phone-outline-2"></use>
+										</svg>	
+										<span class="bc-get-started__button__text">086 362 7302</span>	 	
+									</a><!-- // .bc-get-started__button -->	
+								</span><!-- // .bc-get-started__button-wrap -->	
+							</div><!-- // .bc-get-started__link -->
+						</div><!-- // .bc-get-started__column -->
+						<div class="bc-get-started__column">
+							<div class="bc-get-started__link">
+								<span class="bc-get-started__button-wrap">
+									<a href="https://www.facebook.com/resinbounddesigns"class="bc-get-started__button bc-call-to-action-button">
+										<svg class="svg-icon">
+											<use xlink:href="../assets/media/svg/icons/bc-svgs.svg#facebook"></use>
+										</svg>		
+									<span class="bc-get-started__button__text">Resin Bound Designs Ireland</span>	
+									</a><!-- // .bc-get-started__button -->	
+								</span><!-- // .bc-get-started__button-wrap -->	
+							</div><!-- // .bc-get-started__link -->
+						</div><!-- // .bc-get-started__column -->
+						<div class="bc-get-started__column">
+							<div class="bc-get-started__link">
+								<span class="bc-get-started__button-wrap">
+									<a href="https://www.facebook.com/featuredbespokewallsandceilings"class="bc-get-started__button bc-call-to-action-button">
+										<svg class="svg-icon">
+											<use xlink:href="../assets/media/svg/icons/bc-svgs.svg#facebook"></use>
+										</svg>		
+										<span class="bc-get-started__button__text">Featured Walls and Ceilings</span>
+									</a><!-- // .bc-get-started__button -->	
+								</span><!-- // .bc-get-started__button-wrap -->	
+							</div><!-- // .bc-get-started__link -->
+						</div><!-- // .bc-get-started__column -->
+						<div class="bc-get-started__column">
+							<div class="bc-get-started__link">
+								<span class="bc-get-started__button-wrap">
+									<a href="mailto:capsltd@hotmail.com" class="bc-get-started__button bc-call-to-action-button">
+										<svg class="svg-icon">
+											<use xlink:href="../assets/media/svg/icons/bc-svgs.svg#email-at"></use>
+										</svg>		
+										<span class="bc-get-started__button__text">capsltd@hotmail.com</span> 
+									</a><!-- // .bc-get-started__button -->	
+								</span><!-- // .bc-get-started__button-wrap -->	
+							</div><!-- // .bc-get-started__link -->
+						</div><!-- // .bc-get-started__column -->
+					</div><!-- // bc-grid column-->
+				</article><!-- .bc-grid--x2 -->
+			</section><!-- // Get started -->
 	<footer class="bc-footer bc-container"> 
 		<div class="bc-grid">
 			<div>
